38f10eb60eb785933548bd2d483b9e793ade89b4
Jessica Florey <jhflorey@Jessicas-MacBook-Pro-2.local>
2017-03-30 20:42:19 -0700

    first commit

---
+++ server.js
@@ -0,0 +1,65 @@
+const mongo = require('mongodb').MongoClient;
+const client = require('socket.io').listen(3000).sockets;
+
+// connect to mongodb
+mongo.connect('mongodb://localhost/mongochat', (err, db) => {
+	if(err){
+		throw err;
+	}
+	console.log('Server Started & MongoDB Connected ...');
+	// listen for an event "connection", and we pass on the socket 
+	client.on('connection', (socket) => {
+		let chat = db.collection('chats');
+
+		sendStatus = (s) => {
+			socket.emit('status', s);
+		}
+
+		chat.find().limit(100).sort({_id:1}).toArray((err, res) => {
+			if(err) {
+				throw err;
+			}
+			socket.emit('output', res);
+		});
+
+
+		socket.on('input', (data) => {
+			const name = data.name;
+			const message = data.message;
+
+			if(name == '' || message == '') {
+				sendStatus('Please fill in the name and message');
+			} else {
+				chat.insert({name: name, message: message}, () => {
+					client.emit('output', [data]);
+
+					sendStatus({
+						message: 'message Sent',
+						clear: true
+					});
+				});
+			}
+		});
+
+		socket.on('clear', () => {
+			chat.remove({}, () => {
+				socket.emit('cleared');
+			});
+		});
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
